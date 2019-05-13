db57feaf215b288fabb47a1ff68eecc43c0ecf01
Tawanda Kanyangarara <tawandakanyangarara@tawandas-mbp-2.home>
2019-05-13 10:18:48 +0100

    Compiler

---
+++ Compiler/app.js
@@ -0,0 +1,5 @@
+"use strict";
+var myName = "Max";
+var myAge = 27;
+myName = 30; // even though there is warning - typescirpt will still compile because maybe your code might work
+//# sourceMappingURL=app.js.map
