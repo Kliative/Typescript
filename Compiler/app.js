91683a88e9180a7ca01b08421ba4a2fdbe45c1b8
Tawanda Kanyangarara <tawandakanyangarara@tawandas-mbp-2.home>
2019-05-13 10:25:22 +0100

    changes

--- Compiler/app.js
+++ Compiler/app.js
@@ -1,5 +1,13 @@
 "use strict";
 var myName = "Max";
 var myAge = 27;
-myName = 30; // even though there is warning - typescirpt will still compile because maybe your code might work
+// myName = 30; // even though there is warning - typescirpt will still compile because maybe your code might work
+function controlMe(isTrue, somethingElse) {
+    var result;
+    if (isTrue) {
+        result = 12;
+    }
+    // result = 33; // Stops compilaton error
+    return result;
+}
 //# sourceMappingURL=app.js.map
--- Compiler/app.ts
+++ Compiler/app.ts
@@ -1,5 +1,12 @@
 let myName:string = "Max";
 let myAge: number = 27;
+// myName = 30; // even though there is warning - typescirpt will still compile because maybe your code might work
 
-
-myName = 30; // even though there is warning - typescirpt will still compile because maybe your code might work
+function controlMe(isTrue:boolean, somethingElse: boolean) {
+    let result: number;
+    if (isTrue) {
+        result = 12;
+    }
+    // result = 33; // Stops compilaton error
+    return result;
+}
