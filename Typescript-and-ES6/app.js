34e2500dbd5a00b79760d3830e49ff409bf732af
Tawanda Kanyangarara <tawandakanyangarara@tawandas-mbp-2.home>
2019-05-19 14:42:52 +0100

    Spread & Rest

--- Typescript-and-ES6/app.js
+++ Typescript-and-ES6/app.js
@@ -15,3 +15,36 @@ function reset() {
 }
 reset();
 console.log(variable);
+// Arrow Functions
+console.log("ARROW FUNCTIONS + Variations");
+var addNumbers = function (num1, num2) {
+    return num1 + num2;
+};
+console.log(addNumbers(10, 30));
+var multiplyNum = function (num1, num2) { return num1 * num2; };
+// const multiplyNum = (num1:number,num2:number) => {
+//     return num1 * num2;
+// };
+console.log(multiplyNum(10, 3));
+console.log("Variations");
+// Arrow Functions variations
+var greet = function () {
+    console.log("Hello!");
+};
+greet();
+// [strict Typing Linting] you should see an erorr because you have no explicitly declared a type but its will compile
+var greetFriend = function (friend) { return console.log(friend); };
+greetFriend('Manu');
+// Spread & Rest
+console.log("SPREAD & REST");
+var numbers = [1, 10, 99 - 5];
+console.log(Math.max(33, 99, 44, 5, 62, 90));
+console.log(Math.max.apply(Math, numbers));
+function makeArray(name) {
+    var args = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        args[_i - 1] = arguments[_i];
+    }
+    return args;
+}
+console.log(makeArray("Max", 1, 2, 6));
--- Typescript-and-ES6/app.ts
+++ Typescript-and-ES6/app.ts
@@ -16,3 +16,45 @@ function reset() {
 }
 reset();
 console.log(variable);
+
+// Arrow Functions
+console.log("ARROW FUNCTIONS + Variations");
+const addNumbers = function (num1: number, num2: number): number {
+    return num1 + num2;
+};
+console.log(addNumbers(10, 30));
+
+
+const multiplyNum = (num1: number, num2: number) => num1 * num2;
+// const multiplyNum = (num1:number,num2:number) => {
+//     return num1 * num2;
+// };
+console.log(multiplyNum(10, 3));
+
+console.log("Variations");
+// Arrow Functions variations
+const greet = () => {
+    console.log("Hello!");
+};
+greet();
+// [strict Typing Linting] you should see an erorr because you have no explicitly declared a type but its will compile
+const greetFriend = friend => console.log(friend);
+greetFriend('Manu');
+
+
+// Spread & Rest
+// ...spread is used when passing inot functions
+// turns array in to list of variables
+console.log("SPREAD & REST");
+const numbers = [1, 10, 99 - 5];
+// Math.max example of a funciton that takes a list of numbers
+console.log(Math.max(33, 99, 44, 5, 62, 90));
+console.log(Math.max(...numbers));
+
+// ...rest is used when declaring arguments to be accepted
+// ...rest wiil take all arguments, you have to declare those
+// in which you want the ..rest operater to ignore
+function makeArray(name:string, ...args: number[]) {
+    return args;
+}
+console.log(makeArray("Max", 1, 2, 6));
