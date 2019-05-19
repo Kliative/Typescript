d7e091e666febeac9a2015d0ff781bfeaf736dc0
Tawanda Kanyangarara <tawandakanyangarara@tawandas-mbp-2.home>
2019-05-19 13:12:49 +0100

    block scope

--- Typescript-and-ES6/app.js
+++ Typescript-and-ES6/app.js
@@ -5,3 +5,13 @@ var variable = "Test";
 console.log(variable);
 variable = "Another value";
 console.log(variable);
+var maxLevels = 100;
+console.log(maxLevels);
+// maxLevels = 99; // wont work
+// Block scope
+function reset() {
+    var variable = null;
+    console.log(variable);
+}
+reset();
+console.log(variable);
--- Typescript-and-ES6/app.ts
+++ Typescript-and-ES6/app.ts
@@ -8,3 +8,11 @@ console.log(variable);
 const maxLevels = 100;
 console.log(maxLevels);
 // maxLevels = 99; // wont work
+
+// Block scope
+function reset() {
+    let variable = null;
+    console.log(variable);
+}
+reset();
+console.log(variable);
