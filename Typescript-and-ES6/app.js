7a98ba70859bbf8bb38ff3baf9d9207e7893108b
Tawanda Kanyangarara <tawandakanyangarara@tawandas-mbp-2.home>
2019-05-19 13:11:37 +0100

    const

--- Typescript-and-ES6/app.js
+++ Typescript-and-ES6/app.js
@@ -1,10 +1,7 @@
+"use strict";
 // let & const
 console.log("LET & CONST");
-let variable = "Test";
+var variable = "Test";
 console.log(variable);
 variable = "Another value";
 console.log(variable);
-
-const maxLevels = 100;
-console.log(maxLevels);
-// maxLevels = 99; // wont work
